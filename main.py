05595b4df334d0f1dca08d342e7cbfa2fed770c3
jordan <zjzhang325@gmail.com>
2026-04-06 21:54:40 +0000

    save

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ MOVE_LUT_SIZE = 65536 // 2
 NUM_NTUPLE_MASKS = 8
 SYMMETRIES_PER_MASK = 8
 FEATURES_PER_BOARD = NUM_NTUPLE_MASKS * SYMMETRIES_PER_MASK
+NUM_NTUPLE_VALUES = 16777216
 
 
 @triton.jit
@@ -145,91 +146,43 @@ def ntuple_mask_8(board):
 
 
 @triton.jit
-def emit_all_ntuples(
+def get_all_ntuples(
     boards,
-    output_ptr,
-    output_offsets,
-    output_mask,
 ):
-    board_t = transpose(boards)
-    board_h = flip_horizontal(boards)
-    board_ht = transpose(board_h)
-    board_v = flip_vertical(boards)
-    board_vt = transpose(board_v)
-    board_hv = flip_horizontal(board_v)
-    board_hvt = transpose(board_hv)
+    boards_t = transpose(boards)
+    boards_h = flip_horizontal(boards)
+    boards_ht = transpose(boards_h)
+    boards_v = flip_vertical(boards)
+    boards_vt = transpose(boards_v)
+    boards_hv = flip_horizontal(boards_v)
+    boards_hvt = transpose(boards_hv)
 
-    tl.store(output_ptr + output_offsets + 0, ntuple_mask_1(boards), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 1, ntuple_mask_1(board_t), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 2, ntuple_mask_1(board_h), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 3, ntuple_mask_1(board_ht), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 4, ntuple_mask_1(board_v), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 5, ntuple_mask_1(board_vt), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 6, ntuple_mask_1(board_hv), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 7, ntuple_mask_1(board_hvt), mask=output_mask)
+    nt_1 = ntuple_mask_1(boards)
+    nt_1_t = ntuple_mask_1(boards_t)
+    nt_1_h = ntuple_mask_1(boards_h)
+    nt_1_ht = ntuple_mask_1(boards_ht)
+    nt_1_v = ntuple_mask_1(boards_v)
+    nt_1_vt = ntuple_mask_1(boards_vt)
+    nt_1_hv = ntuple_mask_1(boards_hv)
+    nt_1_hvt = ntuple_mask_1(boards_hvt)
 
-    tl.store(output_ptr + output_offsets + 8, ntuple_mask_2(boards), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 9, ntuple_mask_2(board_t), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 10, ntuple_mask_2(board_h), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 11, ntuple_mask_2(board_ht), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 12, ntuple_mask_2(board_v), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 13, ntuple_mask_2(board_vt), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 14, ntuple_mask_2(board_hv), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 15, ntuple_mask_2(board_hvt), mask=output_mask)
+    nt_2 = ntuple_mask_2(boards)
+    nt_2_t = ntuple_mask_2(boards_t)
+    nt_2_h = ntuple_mask_2(boards_h)
+    nt_2_ht = ntuple_mask_2(boards_ht)
+    nt_2_v = ntuple_mask_2(boards_v)
+    nt_2_vt = ntuple_mask_2(boards_vt)
+    nt_2_hv = ntuple_mask_2(boards_hv)
+    nt_2_hvt = ntuple_mask_2(boards_hvt)
 
-    tl.store(output_ptr + output_offsets + 16, ntuple_mask_3(boards), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 17, ntuple_mask_3(board_t), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 18, ntuple_mask_3(board_h), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 19, ntuple_mask_3(board_ht), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 20, ntuple_mask_3(board_v), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 21, ntuple_mask_3(board_vt), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 22, ntuple_mask_3(board_hv), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 23, ntuple_mask_3(board_hvt), mask=output_mask)
-
-    tl.store(output_ptr + output_offsets + 24, ntuple_mask_4(boards), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 25, ntuple_mask_4(board_t), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 26, ntuple_mask_4(board_h), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 27, ntuple_mask_4(board_ht), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 28, ntuple_mask_4(board_v), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 29, ntuple_mask_4(board_vt), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 30, ntuple_mask_4(board_hv), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 31, ntuple_mask_4(board_hvt), mask=output_mask)
-
-    tl.store(output_ptr + output_offsets + 32, ntuple_mask_5(boards), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 33, ntuple_mask_5(board_t), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 34, ntuple_mask_5(board_h), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 35, ntuple_mask_5(board_ht), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 36, ntuple_mask_5(board_v), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 37, ntuple_mask_5(board_vt), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 38, ntuple_mask_5(board_hv), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 39, ntuple_mask_5(board_hvt), mask=output_mask)
-
-    tl.store(output_ptr + output_offsets + 40, ntuple_mask_6(boards), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 41, ntuple_mask_6(board_t), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 42, ntuple_mask_6(board_h), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 43, ntuple_mask_6(board_ht), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 44, ntuple_mask_6(board_v), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 45, ntuple_mask_6(board_vt), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 46, ntuple_mask_6(board_hv), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 47, ntuple_mask_6(board_hvt), mask=output_mask)
-
-    tl.store(output_ptr + output_offsets + 48, ntuple_mask_7(boards), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 49, ntuple_mask_7(board_t), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 50, ntuple_mask_7(board_h), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 51, ntuple_mask_7(board_ht), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 52, ntuple_mask_7(board_v), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 53, ntuple_mask_7(board_vt), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 54, ntuple_mask_7(board_hv), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 55, ntuple_mask_7(board_hvt), mask=output_mask)
-
-    tl.store(output_ptr + output_offsets + 56, ntuple_mask_8(boards), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 57, ntuple_mask_8(board_t), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 58, ntuple_mask_8(board_h), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 59, ntuple_mask_8(board_ht), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 60, ntuple_mask_8(board_v), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 61, ntuple_mask_8(board_vt), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 62, ntuple_mask_8(board_hv), mask=output_mask)
-    tl.store(output_ptr + output_offsets + 63, ntuple_mask_8(board_hvt), mask=output_mask)
+    nt_3 = ntuple_mask_3(boards)
+    nt_3_t = ntuple_mask_3(boards_t)
+    nt_3_h = ntuple_mask_3(boards_h)
+    nt_3_ht = ntuple_mask_3(boards_ht)
+    nt_3_v = ntuple_mask_3(boards_v)
+    nt_3_vt = ntuple_mask_3(boards_vt)
+    nt_3_hv = ntuple_mask_3(boards_hv)
+    nt_3_hvt = ntuple_mask_3(boards_hvt)
 
 
 @triton.jit
@@ -248,21 +201,13 @@ def move_left(
 
 
 @triton.jit
-def do_all_moves_kernel(
-    boards_ptr,
-    move_lut_ptr,
-    move_rewards_ptr,
-    output_ptr,
-    n_elements,
+def do_all_moves(
+    boards,
+    move_lut,
+    move_rewards,
     BLOCK_SIZE: tl.constexpr,
 ):
-    pid = tl.program_id(axis=0)
-    block_start = pid * BLOCK_SIZE
-    offsets = block_start + tl.arange(0, BLOCK_SIZE)
-    boards = tl.load(boards_ptr + offsets, mask=offsets < n_elements).expand_dims(1)
-    lut_offsets = tl.arange(0, 32768)  # MOVE_LUT_SIZE
-    move_lut = tl.load(move_lut_ptr + lut_offsets)
-    move_rewards = tl.load(move_rewards_ptr + lut_offsets)
+
     move_left_boards, move_left_rewards = move_left(
         boards, move_lut, move_rewards, BLOCK_SIZE
     )
@@ -281,34 +226,49 @@ def do_all_moves_kernel(
         boards_d, move_lut, move_rewards, BLOCK_SIZE
     )
     move_down_boards = transpose(flip_horizontal(move_down_boards))
-    output_offsets = tl.arange(0, BLOCK_SIZE) * 4
+    lr_boards = tl.join(move_left_boards, move_right_boards)
+    ud_boards = tl.join(move_up_boards, move_down_boards)
+    all_boards = tl.join(lr_boards, ud_boards).reshape(BLOCK_SIZE, 4)
+    lr_rewards = tl.join(move_left_rewards, move_right_rewards)
+    ud_rewards = tl.join(move_up_rewards, move_down_rewards)
+    all_rewards = tl.join(lr_rewards, ud_rewards).reshape(BLOCK_SIZE, 4)
+
+    return (
+        all_boards,
+        all_rewards,
+    )
+
+
+@triton.jit
+def do_all_moves_kernel(
+    boards_ptr,
+    move_lut_ptr,
+    move_rewards_ptr,
+    output_ptr,
+    n_elements,
+    BLOCK_SIZE: tl.constexpr,
+):
+    pid = tl.program_id(axis=0)
+    block_start = pid * BLOCK_SIZE
+    offsets = block_start + tl.arange(0, BLOCK_SIZE)
+    boards = tl.load(boards_ptr + offsets, mask=offsets < n_elements).expand_dims(1)
+    lut_offsets = tl.arange(0, 32768)  # MOVE_LUT_SIZE
+    move_lut = tl.load(move_lut_ptr + lut_offsets)
+    move_rewards = tl.load(move_rewards_ptr + lut_offsets)
+    (all_boards, all_rewards) = do_all_moves(boards, move_lut, move_rewards, BLOCK_SIZE)
+    output_offsets = tl.arange(0, BLOCK_SIZE)[:, None] * 4 + tl.arange(0, 4)
     tl.store(
         output_ptr + output_offsets,
-        move_left_boards,
-        mask=output_offsets < n_elements * 4,
-    )
-    tl.store(
-        output_ptr + output_offsets + 1,
-        move_right_boards,
-        mask=output_offsets < n_elements * 4,
-    )
-    tl.store(
-        output_ptr + output_offsets + 2,
-        move_up_boards,
-        mask=output_offsets < n_elements * 4,
-    )
-    tl.store(
-        output_ptr + output_offsets + 3,
-        move_down_boards,
+        all_boards,
         mask=output_offsets < n_elements * 4,
     )
 
 
-def do_all_moves(
+def do_all_moves_wrapper(
     boards: torch.Tensor, move_lut: torch.Tensor, move_rewards: torch.Tensor
 ) -> torch.Tensor:
     output = torch.empty((boards.shape[0], 4), device=DEVICE, dtype=torch.uint64)
-    do_all_moves_kernel[1, 1](
+    do_all_moves_wrapper[1, 1](
         boards, move_lut, move_rewards, output, boards.shape[0], BLOCK_SIZE=1024
     )
     return output
@@ -329,9 +289,16 @@ def train_epoch_kernel(
     offsets = block_start + tl.arange(0, BLOCK_SIZE)
     active = offsets < n_elements
     boards = tl.load(boards_ptr + offsets, mask=active)
+    move_lut = tl.load(move_lut_ptr + offsets)
+    move_rewards = tl.load(move_rewards_ptr + offsets)
     output_offsets = offsets * FEATURES_PER_BOARD
+    for i in range(FEATURES_PER_BOARD):
+        (
+            all_boards,
+            all_rewards,
+        ) = do_all_moves_kernel(boards, move_lut, move_rewards, BLOCK_SIZE)
+        valid_move = all_boards != boards.expand_dims(1)
     # Output layout is [mask_0_sym_0..7, mask_1_sym_0..7, ..., mask_7_sym_0..7].
-    emit_all_ntuples(boards, output_ptr, output_offsets, active)
 
 
 @triton.jit
@@ -470,5 +437,6 @@ print_boards(repr_to_board(boards))
 print("-" * 100)
 
 move_lut, move_rewards = generate_transition_tables(DEVICE)
+ntuple_values = torch.zeros((8, NUM_NTUPLE_VALUES), device=DEVICE, dtype=torch.float32)
 all_moves = do_all_moves(boards, move_lut, move_rewards)
 print_boards(repr_to_board(all_moves[1, :]))
